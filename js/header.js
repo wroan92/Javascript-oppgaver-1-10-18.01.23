8a4c88c34dd62c55de6cc69c776f01f7e5ac21fb
wroan92 <Anders-wroldsen@live.com>
2023-01-25 22:25:17 +0100

    First try, did not work....

---
+++ js/header.js
@@ -0,0 +1,13 @@
+const menu = document.createElement("header")
+menu.id = "header"
+
+const ul = document.createElement("ul")
+const oppgave1 = document.createElement("li");
+const oppgave1Link = document.createElement("a");
+oppgave1Link.href = "oppgave1.html";
+oppgave1Link.innerHTML = "Oppgave 1"
+oppgave1.appendChild(Oppgave1Link);
+
+ul.appendChild(ul)
+
+document.body.appendChild(header)
