7616ceab5a4993892b36e0c0f191ae635579b934
wroan92 <Anders-wroldsen@live.com>
2023-01-25 21:20:39 +0100

    Finish task 3

--- js/oppgave3.js
+++ js/oppgave3.js
@@ -1,17 +1,31 @@
-const cardBtn = document.querySelector("#makePersonasBtn")
+const cardContainer = document.querySelector(".hide");
+const inputContainer = document.querySelector(".inputContainer");
+
 
 document.querySelector("#makePersonasBtn").addEventListener("click", event =>  {
+
+    cardContainer.classList.replace("hide", "cardContainer");
+    inputContainer.classList.replace("inputContainer", "hide");
+
+
     const firstName = document.querySelector("#firstNameInput").value;
     const lastName = document.querySelector("#lastNameInput").value;
     const age = document.querySelector("#ageInput").value;
     const city = document.querySelector("#cityInput").value;
     const country = document.querySelector("#countryInput").value;
-    
+
     document.querySelector("#outputContainer").innerHTML = 
+
     `Navn: ${firstName} ${lastName} <br>
      Alder: ${age} <br>
      Bosted: ${city} <br>
      Land: ${country}`;
+     
   });
-  
-  
+
+document.querySelector("#tryAgainBtn").addEventListener("click", event => {
+   
+    cardContainer.classList.replace("cardContainer", "hide");
+    inputContainer.classList.replace("hide", "inputContainer");
+    
+});
