d02698a0fa9fd85753f19120edc2a5ee19d997fd
wroan92 <Anders-wroldsen@live.com>
2023-01-27 11:31:10 +0100

    JS Navbar working

--- js/header.js
+++ js/header.js
@@ -22,10 +22,38 @@ document.addEventListener("DOMContentLoaded", function() {
   taskTwoLink.innerText = "Oppgave 2";
   taskTwo.appendChild(taskTwoLink);
   
+  const taskThre = document.createElement("li")
+  const taskThreLink = document.createElement("a")
+  taskThreLink.href = "./oppgave3.html"
+  taskThreLink.innerText = "Oppgave 3"
+  taskThre.appendChild(taskThreLink)
   
+  const taskFour = document.createElement("li")
+  const taskFourLink = document.createElement("a")
+  taskFourLink.href = "./oppgave4.html"
+  taskFourLink.innerText = "Oppgave 4"
+  taskFour.appendChild(taskFourLink)
+  
+  const taskFive = document.createElement("li")
+  const taskFiveLink = document.createElement("a")
+  taskFiveLink.href = "./oppgave5.html"
+  taskFiveLink.innerText = "Oppgave 5"
+  taskFive.appendChild(taskFiveLink)
+  
+  const taskSix = document.createElement("li")
+  const taskSixLink = document.createElement("a")
+  taskSixLink.href = "./oppgave6.html"
+  taskSixLink.innerText = "Oppgave 6"
+  taskSix.appendChild(taskSixLink)
+  
+
   navbar.appendChild(home)
   navbar.appendChild(taskOne);
   navbar.appendChild(taskTwo);
+  navbar.appendChild(taskThre)
+  navbar.appendChild(taskFour)
+  navbar.appendChild(taskFive)
+  navbar.appendChild(taskSix)
   
   nav.appendChild(navbar);
   navbarContainer.appendChild(nav);
