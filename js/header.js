ea49fa7330c723e6b53d0968839e32524c25ceba
wroan92 <Anders-wroldsen@live.com>
2023-02-13 21:51:41 +0100

    Change all file names, navbar working as forloop

--- js/header.js
+++ js/header.js
@@ -1,58 +1,21 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const navbarContainer = document.querySelector("header")
-  
-  const nav = document.createElement("nav");
-  const navbar = document.createElement("ul");
+const navbarContainer = document.querySelector("header");
 
-  const homeBtn = document.createElement("a")
-  homeBtn.className = "homeBtn"
-  homeBtn.href = "./index.html";
-  homeBtn.innerText = "Hjem"
-  
-  const taskOne = document.createElement("li");
-  const taskOneLink = document.createElement("a");
-  taskOneLink.href = "./oppgave1.html";
-  taskOneLink.innerText = "Oppgave 1";
-  taskOne.appendChild(taskOneLink);
-
-  const taskTwo = document.createElement("li");
-  const taskTwoLink = document.createElement("a");
-  taskTwoLink.href = "./oppgave2.html";
-  taskTwoLink.innerText = "Oppgave 2";
-  taskTwo.appendChild(taskTwoLink);
-  
-  const taskThre = document.createElement("li")
-  const taskThreLink = document.createElement("a")
-  taskThreLink.href = "./oppgave3.html"
-  taskThreLink.innerText = "Oppgave 3"
-  taskThre.appendChild(taskThreLink)
-  
-  const taskFour = document.createElement("li")
-  const taskFourLink = document.createElement("a")
-  taskFourLink.href = "./oppgave4.html"
-  taskFourLink.innerText = "Oppgave 4"
-  taskFour.appendChild(taskFourLink)
-  
-  const taskFive = document.createElement("li")
-  const taskFiveLink = document.createElement("a")
-  taskFiveLink.href = "./oppgave5.html"
-  taskFiveLink.innerText = "Oppgave 5"
-  taskFive.appendChild(taskFiveLink)
-  
-  const taskSix = document.createElement("li")
-  const taskSixLink = document.createElement("a")
-  taskSixLink.href = "./oppgave6.html"
-  taskSixLink.innerText = "Oppgave 6"
-  taskSix.appendChild(taskSixLink)
-  
-  navbarContainer.appendChild(homeBtn);
-  navbar.appendChild(taskOne);
-  navbar.appendChild(taskTwo);
-  navbar.appendChild(taskThre);
-  navbar.appendChild(taskFour);
-  navbar.appendChild(taskFive);
-  navbar.appendChild(taskSix);
-  
-  nav.appendChild(navbar);
+createNav = () => {
+const nav = document.createElement("nav");
+const ul = document.createElement("ul");
   navbarContainer.appendChild(nav);
-});
+  nav.appendChild(ul);
+
+// Add home button
+const homeBtn = document.createElement("li");
+  homeBtn.innerHTML = `<a href="./index.html">Hjem</a>`;
+  ul.appendChild(homeBtn);
+
+  for (let i = 1; i <= 6; i++) {
+const newLi = document.createElement("li");
+    newLi.innerHTML = `<a href="./task${i}.html">Oppgave ${i}</a>`;
+    ul.appendChild(newLi);
+  }
+};
+
+createNav();
